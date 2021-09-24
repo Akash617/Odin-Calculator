2732596398cb5ddc43ef2acedfefd572d7e8ae5b
Akash617 <akash617@yahoo.com>
2021-09-24 05:59:29 -0400

    Addition, subtraction and multiply now work

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
 function add() {
     result = number1 + number2;
+    number1 = result;
+    resetInput();
 }
 
 function sub() {
     result = number1 - number2;
+    number1 = result;
+    resetInput();
 }
 
 function mul() {
     result = number1*number2;
+    number1 = result;
+    resetInput();
 }
 
 function div() {
-
+    resetInput();
 }
 
 function resetInput() {
-    number1 = result;
-    result, number2 = -1;
-    reset = false;
+    result, number2 = 0;
+    firstOp = false;
+    displayMain(number1);
 }
 
 function resetAll() {
-    number1, number2, result = -1;
-    reset = true;
+    number1, number2, result = 0;
+    firstOp = true;
 }
 
 function displayMain(value) {
-
+    resDisplay.innerHTML = value;
 }
 
 function inputNum(value) {
-    console.log(`inputting ${value}`);
-    if (reset) {
-        number1 = value;
+    if (firstOp) {
+        number1Tmp = number1;
+        number1 = (number1Tmp*10) + value;
+        number1Tmp = 0;
+        displayMain(number1);
     } else {
-        // Whenever num2 has a value, it means the operation can be done so run() is called
-        number2 = value;
-        run();
+        number2Tmp = number2;
+        number2 = (number2Tmp*10) + value;
+        number2Tmp = 0;
+        displayMain(number2);
     }
 }
 
+function operation() {
+
+}
+
 function run() {
+    
     if (addBit) {
         add();
         addBit = false;
@@ -53,15 +67,21 @@ function run() {
     } else if (divBit) {
         div();
         divBit = false;
+    } else {
+        resetInput();
     }
 }
 
-let number1 = -1;
-let number2 = -1;
-let result = -1;
-let reset = true; //Used in case the actual value of num1 is -1 because of the resetInput() function
+let number1 = 0;
+let number1Tmp = 0;
+let number2 = 0;
+let number2Tmp = 0;
+let result = 0;
+let firstOp = true; //Used in case the actual value of num1 is -1 because of the resetInput() function
 
 let addBit, subBit, mulBit, divBit = false;
+
+let resDisplay = document.querySelector(".res-display");
 
 let inNum0 = document.querySelector(".num0");
 let inNum1 = document.querySelector(".num1");
@@ -96,3 +116,25 @@ inNum6.addEventListener('click', function() {inputNum(6)});
 inNum7.addEventListener('click', function() {inputNum(7)});
 inNum8.addEventListener('click', function() {inputNum(8)});
 inNum9.addEventListener('click', function() {inputNum(9)});
+
+opAdd.addEventListener('click' , function() {
+    run();
+    addBit = true;
+});
+
+opSub.addEventListener('click' , function() {
+    run();
+    subBit = true;
+});
+
+opMul.addEventListener('click' , function() {
+    run();
+    mulBit = true;
+});
+
+opDiv.addEventListener('click' , function() {
+    run();
+    divBit = true;
+});
+
+opEq.addEventListener('click' , run);
